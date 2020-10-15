f2d3d244f718763db2057748b0280df9ec4565cf
losdre <wilsonrivero2002@gmail.com>
2020-10-15 13:09:33 -0300

    add everthing new

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const handlebars = require('express-handlebars')
 const bodyParser =require('body-parser')
 const app = new express()
 const admin = require('./routes/admin')//here we are calling the route
+const path = require('path') //this is for to manipulate folders
+const { join } = require('path')
+
 //const mongosse = require ('mongosse')
 
 //CONFIG
@@ -12,11 +15,14 @@ const admin = require('./routes/admin')//here we are calling the route
     app.use(bodyParser.json())
     
     //HANDLESBARS
-    app.engine('handlebras', handlebars({defaultLayout: 'main'}))
+    app.engine('handlebars', handlebars({defaultLayout: 'main'}))
     app.set('view engine', 'handlebars')
 
+    //Public
+    app.use(express.static(path.join(__dirname,'public')))//here we show the folder about front-end 
+
 //routes
-    app.use('/adm', admin)//"/admin"  you have to use a prexi
+    app.use('/admin',admin)//"/admin"  you have to use a prexi
 
 //outhers
 const PORT = 3000
--- routes/admin.js
+++ routes/admin.js
@@ -3,11 +3,11 @@ const router = express.Router()//this is for export routs from another files
 
 
 router.get('/', (req, res) =>{
-   res.send('Page main')
+    res.render('admin/index')
 })
 
 router.get('/post', (req,res) =>{
-    res.send('Page of the post')
+    res.render('admin/test')
 })
 router.get('/categories',(req,res)=>{
     res.send('Page categories')
