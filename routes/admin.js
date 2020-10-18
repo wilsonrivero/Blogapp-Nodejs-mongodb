435aca814020eb2290fa3e77210bb2e59c163493
losdre <wilsonrivero2002@gmail.com>
2020-10-18 15:03:03 -0300

    add function delete

--- routes/admin.js
+++ routes/admin.js
@@ -15,7 +15,7 @@ router.get('/post', (req,res) =>{
     res.send('Page blog')
 })
 router.get('/categories',(req,res)=>{
-    Categorie.find().then((categories) =>{
+    Categorie.find().sort({date: 'desc'}).then((categories) =>{
         res.render('admin/categories',{categories: categories.map(category => category.toJSON())}) 
     }).catch((erro) =>{
         req.flash('erro_msg', 'Houver erro em cadatrar a categoria')
@@ -57,8 +57,44 @@ router.post('/categories/new', (req,res) =>{
             res.redirect('/admin')
         })
     }
-
-
 })
 
+router.get('/categories/edit/:id', (req,res) =>{
+    Categorie.findOne({_id:req.params.id}).lean().then((categorie) =>{
+        res.render('admin/editcategories', {categorie: categorie})
+    }).catch((err) =>{
+        req.flash('erro_msg', 'HOuve erro')
+        res.redirect('/amdin/categories')
+    })
+   
+})
+
+router.post('/categories/edit',(req,res) =>{
+    Categorie.findOne({_id: req.body.id}).then((categoire) =>{
+        categoire.nome = req.body.nome
+        categoire.slug = req.body.slug
+
+        categoire.save().then(()=>{
+            req.flash('success_msg', 'categoria editada')
+            res.redirect('/admin/categories')
+        }).catch((err) =>{
+            req.flash('erro_msg', 'houve errp')
+            res.redirect('/admin/categories')
+        })
+    }).catch((err) =>{
+        req.flash('erro_msg', 'houve erro')
+        res.redirect('/admin/categories')
+    })
+})
+
+
+router.post('/categories/delete', (req,res) =>{
+    Categorie.remove({_id: req.body.id}).then(()=>{
+        req.flash('success_msg', 'Categoria deleteada')
+        res.redirect('/admin/categories')
+    }).catch((err) =>{
+        req.flash('erro_msg','Erro ao deletar' +err )
+        res.redirect('/admin/categories')
+    })
+})
 module.exports = router
